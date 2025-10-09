4915e0fa0c401712fecd236993414ca31aa7b96f
Marion <mariontamnai@gmail.com>
2025-10-10 00:42:07 +0300

    updated horizontal MovieRow to one

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import "./App.css";
 import {  fetchTrendingMovies, fetchMovies } from "./services/movieApi";
 import SearchBar from "./components/SearchBar";
 import MovieCard from "./components/MovieCard";
+import MovieRow from "./components/MovieRow";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    const data = await fetchMovies(query);
-    setMovies(data);
-    setLoading(false);
-  };
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  
 
   useEffect(() => {
     const loadTrending = async () => {
@@ -24,6 +21,15 @@ function App() {
     };
     loadTrending();
   }, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (query.trim() === "") return;
+    setLoading(true);
+    const data = await fetchMovies(query);
+    setMovies(data);
+    setLoading(false);
+  };
 
   // (Live Search)
   useEffect(() => {
@@ -46,13 +52,13 @@ function App() {
   <div className="bg-black min-h-screen text-white">
     <header className="p-4 flex justify-center">
       
-      <SearchBar value={query} onChange={setQuery} />
+      <SearchBar value={query} onChange={setQuery} onSubmit={handleSearch} />
     </header>
 
     <main className="p-4">
-      {loading ? (
-        <p>Loading...</p>
-      ) : movies.length > 0 ? (
+      {loading && <p>Loading...</p>}
+      
+      {query && movies.length > 0 && (
         <div>
           <h2 className="text-xl font-semibold mb-2">{query ? "Search Results" : "Trending Movies"}</h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
@@ -61,7 +67,13 @@ function App() {
             ))}
           </div>
         </div>
-      ) : (
+      )}
+
+      {!query && trendingMovies.length > 0 && (
+        <MovieRow title="Trending Movies" movies={trendingMovies} />
+      )}
+
+      {!loading && movies.length === 0 && !query && (
         <p className="text-gray-400">Search for a movie to begin...</p>
       )}
     </main>
