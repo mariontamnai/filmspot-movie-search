75c1b9c476df608300966508015d488f6e7bd7dc
Marion <mariontamnai@gmail.com>
2025-10-09 15:54:50 +0300

    updated app.js for APItesting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-import "./App.css"
+import React, { useEffect, useState} from "react";
+import "./App.css";
+import { fetchMovies } from ".services/movieApi";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      const data = await fetchMovies("avengers");
+      setMovies(data);
+    };
+    loadMovies();
+  }, []);
   return (
     <div className="App">
-      <h1>FilmSpot Movie Search</h1>
+      <h1>FilmSpot</h1>
       <p>Search and discover your favorite movies here!</p>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.tittle}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 export default App
