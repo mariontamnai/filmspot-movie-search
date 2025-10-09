426f830878b3896c974f7a216fedec5c9a698f02
Marion <mariontamnai@gmail.com>
2025-10-09 16:49:45 +0300

    updated movieApi.js to display trending movies by default

--- src/services/movieApi.js
+++ src/services/movieApi.js
@@ -1,6 +1,17 @@
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
+export const fetchTrendingMovies = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching trending movies:", error);
+    return [];
+  }
+};
+
 export const fetchMovies = async (query) => {
   try {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
