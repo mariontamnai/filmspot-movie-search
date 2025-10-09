d56ea1ec231f55b6459b45cca93a7dced1d0e266
Marion <mariontamnai@gmail.com>
2025-10-09 16:42:21 +0300

    updated App.css/js to display real time data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState} from "react";
 import "./App.css";
-import { fetchMovies } from ".services/movieApi";
+import { fetchMovies } from "./services/movieApi";
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const loadMovies = async () => {
-      const data = await fetchMovies("avengers");
-      setMovies(data);
+      const data = await fetchMovies("Mean Girls");
+      console.log(data.results[0]);
+      setMovies(data.results || []);
     };
     loadMovies();
   }, []);
@@ -17,10 +18,14 @@ function App() {
       <h1>FilmSpot</h1>
       <p>Search and discover your favorite movies here!</p>
       <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>{movie.tittle}</li>
-        ))}
-      </ul>
+  {movies.map((movie) => (
+    <li key={movie.id}>
+      {movie.title || movie.name || "No title"}{" "}
+      ({movie.release_date ? movie.release_date.slice(0, 4) : "N/A"})
+    </li>
+  ))}
+</ul>
+
     </div>
   );
 }
