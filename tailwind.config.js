c9dc0870e44ce20491d744894b335a8a64a51106
Marion <mariontamnai@gmail.com>
2025-10-21 11:53:09 +0300

    updated the whole structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
+  plugins: [],
 }
